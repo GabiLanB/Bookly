12b11bf63c9396dbd9483d353853131bb0d52c6f
Gabriel <gabriel.balan@sabs.ro>
2018-12-11 14:07:14 +0200

    stupid error

--- Bookly/Controllers/BooksController.cs
+++ Bookly/Controllers/BooksController.cs
@@ -1,8 +1,9 @@
-﻿
+﻿using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 using System.Web.Mvc;
+using Bookly.Migrations;
 using Bookly.Models;
 using Bookly.ViewModels;
 
@@ -24,10 +25,39 @@ namespace Bookly.Controllers
 
         public ViewResult Index()
         {
-            var books = _context.Books.Include(m => m.Genre).ToList(); ;
+            var books = _context.Books.Include(m => m.Genre).ToList();
 
             return View(books);
         }
+
+        public ViewResult New()
+        {
+            var genres = _context.Genres.ToList();
+
+            var viewModel = new BookFormViewModel
+            {
+                Genres = genres
+            };
+
+            return View("BookForm", viewModel);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var book = _context.Books.SingleOrDefault(c => c.Id == id);
+
+            if (book == null)
+                return HttpNotFound();
+
+            var viewModel = new BookFormViewModel
+            {
+                Book = book,
+                Genres = _context.Genres.ToList()
+            };
+
+            return View("BookForm", viewModel);
+        }
+
 
         public ActionResult Details(int id)
         {
@@ -57,6 +87,28 @@ namespace Bookly.Controllers
             };
 
             return View(viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Save(Book book)
+        {
+            if (book.Id == 0)
+            {
+                book.DateAdded = DateTime.Now;
+                _context.Books.Add(book);
+            }
+            else
+            {
+                var bookInDb = _context.Books.Single(m => m.Id == book.Id);
+                bookInDb.Name = book.Name;
+                bookInDb.GenreId = book.GenreId;
+                bookInDb.NumberInStock = book.NumberInStock;
+                bookInDb.ReleaseDate = book.ReleaseDate;
+            }
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Books");
         }
     }
 }
